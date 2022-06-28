4775b12f1ba39049121838a5434064cb4d574439
Alok Raj <alokraj7184@gmail.com>
2022-06-28 21:24:10 +0530

    Few More changes

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('', views.index),
+    # path('dayFive/', views.dayFive),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,13 +1,23 @@
+from email import message
 from django.shortcuts import render
 import requests
 import datetime
 
+city = 'Gharuan'
+
+lon = 102
+lat = 104
+
 
 def index(request):
     if 'city' in request.POST:
-        city = request.POST['city']
+        if request.POST['city'] == "":
+            city = 'Gharuan'
+        else:
+            city = request.POST['city']
+
     else:
-        city = 'Gaya'
+        city = 'Gharuan'
     appid = 'a14edc84ee129b22c119aaee2bc8d70a'
     URL = 'https://api.openweathermap.org/data/2.5/weather'
     PARAMS = {'q': city, 'appid': appid, 'units': 'metric'}
@@ -16,7 +26,24 @@ def index(request):
     description = res['weather'][0]['description']
     icon = res['weather'][0]['icon']
     temp = res['main']['temp']
-
+    pressure = res['main']['pressure']
+    humidity = res['main']['humidity']
+    clouds = res['clouds']['all']
+    country = res['sys']['country']
+    lon = res['coord']['lon']
+    lat = res['coord']['lat']
     day = datetime.date.today()
 
-    return render(request, 'weatherApp/index.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
+    return render(request, 'weatherApp/index.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city, 'pressure': pressure, 'humidity': humidity, 'clouds': clouds, 'country': country})
+
+
+# def dayFive(request):
+#     appid = 'a14edc84ee129b22c119aaee2bc8d70a'
+#     URL = 'https://api.openweathermap.org/data/2.5/forecast/daily'
+#     PARAMS = {'lat': lat, 'lon': lon, 'cnt': 5,
+#               'appid': appid, 'units': 'metric'}
+#     r = requests.get(url=URL, params=PARAMS)
+#     res = r.json()
+#     citys = res['city']['name']
+#     temp = res['list'][0]['main']['temp']
+#     return render(request, 'weatherApp/Day5.html', {'citys': citys, 'temp': temp})
